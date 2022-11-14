412f35e34ae6a89a8964b01d9d837058f564e346
Lucas <lucasfanjos0022@gmail.com>
2022-11-14 15:35:24 -0300

    homepage incluida

--- src/App.js
+++ src/App.js
@@ -5,18 +5,27 @@ import Jobs from "./Components/Jobs/Jobs";
 function App() {
   return (
     <div className="App">
-      <ul className="circles">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
+      <header>
+        <div className="imgBox">
+          <img src={logo} className="logo" />
+        </div>
+        <nav>
+          <ul>
+            <li>Lorem</li>|
+            <li>Lorem</li>|
+            <li>Lorem</li>|
+            <li>Lorem</li>|
+            <li>Lorem</li>
+          </ul>
+          <div className="navButtons">
+            <button id="entrar">Entrar</button>
+            <button id="cadastrar">Cadastrar</button>
+          </div>
+        </nav>
+      </header>
+      <div className="home">
+        <h1 className="homeText">Ache o trampo <g>perfeito</g><br/> com a <g>QuickJobs</g></h1>
+      </div>
       <Jobs></Jobs>
     </div>
   );
